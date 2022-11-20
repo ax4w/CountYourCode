94d09a31fb8b69bfe9eb3b5bd6f6012d27b7b667
Alex <a-wrede@outlook.de>
2022-11-20 21:37:19 +0100

    clean up

--- main.go
+++ main.go
@@ -11,75 +11,70 @@ import (
 	"sync"
 )
 
-type (
-	/*
-		struct to lock and unlock to prevent concurrent map writes
-	*/
-	linesEachFile struct {
-		mutex            sync.Mutex
-		linesEachFileMap map[string]int
-	}
-)
+type lines struct {
+	linesEachFile map[string]int
+	total         int
+}
 
 var (
-	excludeFileTypes []string
-	excludeDirs      []string
-	scannerBuffer    *int
-	totalSum         = 0
-	wg               sync.WaitGroup
+	scannerBuffer *int
+	wg            sync.WaitGroup
 )
 
-func (linesEachFile *linesEachFile) readFile(path string) {
+func readFile(path string, c chan int) {
 	file, err := os.Open(path)
 	defer file.Close() //close when done
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 	defer wg.Done() //tell waitgroup that routine is done
+	total := 0
 	scanner := bufio.NewScanner(file)
 	buf := make([]byte, *scannerBuffer)
 	scanner.Buffer(buf, *scannerBuffer)
 	scanner.Split(bufio.ScanLines)
-
-	linesEachFile.mutex.Lock()         //lock for map writes
-	defer linesEachFile.mutex.Unlock() //unlock when done
-	linesEachFile.linesEachFileMap[path] = 0
 	for scanner.Scan() {
 		if len(scanner.Text()) > 0 { //skip if it's just an empty line
-			linesEachFile.linesEachFileMap[path]++
-			totalSum++
+			total++
 		}
 	}
+	c <- total //send total back through channel
 }
 
-func (linesEachFile *linesEachFile) iterateOverDir(path string) {
+func (lines *lines) iterateOverDir(path string,
+	excludeDirs []string,
+	excludeFileTypes []string) {
 	filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
 		skip := false
 		if err != nil {
 			log.Fatalf(err.Error())
 		}
-
-		if !f.IsDir() {
-			for _, e := range excludeDirs {
-				if strings.Contains(path, e) {
+		if f.IsDir() {
+			return nil
+		}
+		for _, e := range excludeDirs {
+			if strings.Contains(path, e) {
+				skip = true
+			}
+		}
+		if skip {
+			return nil
+		}
+		extension := strings.Split(f.Name(), ".")
+		if len(extension) > 1 {
+			for _, e := range excludeFileTypes {
+				if e == extension[1] {
 					skip = true
+					break
 				}
 			}
+			c := make(chan int)
 			if !skip {
-				extension := strings.Split(f.Name(), ".")
-				if len(extension) > 1 {
-					for _, e := range excludeFileTypes {
-						if e == extension[1] {
-							skip = true
-							break
-						}
-					}
-				}
-
-				if !skip {
-					wg.Add(1)
-					go linesEachFile.readFile(path)
-				}
+				wg.Add(1)
+				go readFile(path, c)
+				currFile := <-c
+				lines.linesEachFile[path] = currFile
+				lines.total += currFile
 			}
 			skip = false
 
@@ -88,18 +83,19 @@ func (linesEachFile *linesEachFile) iterateOverDir(path string) {
 	})
 }
 
-func (linesEachFile *linesEachFile) printResult() {
-	if len(linesEachFile.linesEachFileMap) == 0 || totalSum == 0 {
+func (lines *lines) printResult() {
+	if len(lines.linesEachFile) == 0 || lines.total == 0 {
 		fmt.Println("No files were read!")
 		return
 	}
-	for k, e := range linesEachFile.linesEachFileMap {
+	for k, e := range lines.linesEachFile {
 		fmt.Printf("File %s contained %d lines\n", k, e)
 	}
-	fmt.Printf("This project has a total lenght of %d lines\n", totalSum)
+	fmt.Printf("This project has a total lenght of %d lines\n", lines.total)
 }
 
 func main() {
+
 	dirFlag := flag.String(
 		"dir",
 		"None",
@@ -124,11 +120,13 @@ func main() {
 	if *dirFlag == "None" {
 		log.Fatalf("No directory was given!\nRun -help to see all options")
 	}
-	excludeFileTypes = strings.Split(*excludeFileFlag, ";")
-	excludeDirs = strings.Split(*excludeDirsFlag, ";")
-	lef := linesEachFile{}
-	lef.linesEachFileMap = make(map[string]int)
-	lef.iterateOverDir(*dirFlag)
+	lines := lines{
+		linesEachFile: make(map[string]int),
+		total:         0,
+	}
+	excludeFileTypes := strings.Split(*excludeFileFlag, ";")
+	excludeDirs := strings.Split(*excludeDirsFlag, ";")
+	lines.iterateOverDir(*dirFlag, excludeDirs, excludeFileTypes)
 	wg.Wait() //wait for all routines to finish
-	lef.printResult()
+	lines.printResult()
 }
